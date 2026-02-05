832213064c672bf12dcaa573a669b2e5e877fdfb
Rahul Tiwari <rahul.rkt007@gmail.com>
2026-02-05 23:12:44 +0530

    congire in playwright.js for test run on chromium

--- playwright.config.js
+++ playwright.config.js
@@ -45,7 +45,8 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
+    }
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -53,6 +54,6 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },*/
   ],
 });
